341e3fd811370cbae524763c3d7ef9a52620c3da
FlynnMager <flynnmager@gmail.com>
2023-02-23 15:10:13 -0800

    Added Flynn.js, displays through index.js
    I added another root iin index.js, and had it grab the function from
    the new Flynn.js, and output it to the div with id Flynn

---
+++ housing/src/Flynn.js
@@ -0,0 +1,13 @@
+function test1() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Flynn testing here
+          </p>
+        </header>
+      </div>
+    );
+  }
+  
+  export default test1;
