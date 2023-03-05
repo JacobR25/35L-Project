6f2ec00fe67d5819376682ae03c9fc322ffeb308
FlynnMager <flynnmager@gmail.com>
2023-03-04 18:52:28 -0800

    Added map image, and basic styling to it
    finally adding the actual map picture, going to decide how to more
    foward with counties next

--- housing/src/Flynn.js
+++ housing/src/Flynn.js
@@ -1,8 +1,11 @@
+import map from './images/CAMap.png';
+
 function test1() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
+            <img src={map} style={{ width:`500px` }} alt="map of cali"/>
             Flynn testing here
           </p>
         </header>
