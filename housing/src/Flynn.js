9bcd9fbff1861d3423fc8162a34363007ccb61c3
FlynnMager <flynnmager@gmail.com>
2023-03-04 21:50:36 -0800

    Created interactive map with react components
    I did a lot here in this commit:
         -added json database of california county data
         -implemeted an interactive map such that when you hover a county,
         the county name is displayed at the bottom of the screen
         -yeah thats about it

--- housing/src/Flynn.js
+++ housing/src/Flynn.js
@@ -1,8 +1,56 @@
 import map from './images/CAMap.png';
+import React from 'react';
+import './App.css';
+
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+} from 'react-simple-maps';
+import californiaCounties from './California_County_Boundaries.geojson';
 const ImageStyle={
   width:`500px`,
   float:'right'
 }
+
+
+const CaliforniaMap = () => {
+  const [hoverName, setTooltipContent] = React.useState('No county hovered');
+
+  const handleCountyHover = (CountyName) => {
+    console.log(CountyName);
+    setTooltipContent(CountyName);
+  };
+
+  const handleCountyLeave = () => {
+    setTooltipContent('No county hovered');
+  };
+
+  return (
+    <div>
+      <ComposableMap projection="geoAlbersUsa">
+        <Geographies geography={californiaCounties}>
+          {({ geographies }) =>
+            geographies.map((geo) => (
+              <Geography
+                key={geo.rsmKey}
+                geography={geo}
+                onMouseEnter={() => handleCountyHover(geo.properties.CountyName)}
+                onMouseLeave={handleCountyLeave}
+                fill="pink"
+                stroke="#FFF"
+                strokeWidth={0.5}
+              />
+            ))
+          }
+        </Geographies>
+      </ComposableMap>
+      <div>The county you are hovering is: {hoverName}</div>
+    </div>
+  );
+};
+
+
 function test1() {
     return (
       <div className="App">
@@ -11,7 +59,9 @@ function test1() {
             <img src={map} style={ImageStyle} alt="map of cali"/>
             Flynn testing here
           </p>
+          
         </header>
+        <div ><CaliforniaMap/></div>
       </div>
     );
   }
