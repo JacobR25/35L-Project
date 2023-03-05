ef95db3433f86c53eed2f396669f8a20d7747bca
FlynnMager <flynnmager@gmail.com>
2023-03-04 20:05:39 -0800

    Changes styling on image to separate object

--- housing/src/Flynn.js
+++ housing/src/Flynn.js
@@ -1,11 +1,14 @@
 import map from './images/CAMap.png';
-
+const ImageStyle={
+  width:`500px`,
+  float:'right'
+}
 function test1() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            <img src={map} style={{ width:`500px` }} alt="map of cali"/>
+            <img src={map} style={ImageStyle} alt="map of cali"/>
             Flynn testing here
           </p>
         </header>
