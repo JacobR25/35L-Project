025dc28d66221069a33a6e82cc10e35ceedce6b3
FlynnMager <flynnmager@gmail.com>
2023-03-05 20:17:16 -0800

    Got rid of image and resized map
    I completely deleted the image implementation, and
    moved the map into a better place on the screen,
    as well as the county you are hovering feature

--- housing/src/Flynn.js
+++ housing/src/Flynn.js
@@ -1,4 +1,3 @@
-import map from './images/CAMap.png';
 import React from 'react';
 import './App.css';
 
@@ -8,44 +7,64 @@ import {
   Geography,
 } from 'react-simple-maps';
 import californiaCounties from './California_County_Boundaries.geojson';
-const ImageStyle={
-  width:`500px`,
-  float:'right'
-}
+
 
 
 const CaliforniaMap = () => {
   const [hoverName, setTooltipContent] = React.useState('No county hovered');
-
+  const [clickedName, setClickedName] = React.useState('No county clicked yet');
   const handleCountyHover = (CountyName) => {
     console.log(CountyName);
     setTooltipContent(CountyName);
   };
-
+  const handleMouseDown = (CountyName) => {
+    //call function to display the county rent info
+    setClickedName(CountyName);
+  };
   const handleCountyLeave = () => {
     setTooltipContent('No county hovered');
   };
-
+  const isHovered = (name) => {
+    if(name===hoverName){
+      return 'red';
+    }
+    return 'pink';
+  }
   return (
-    <div>
-      <ComposableMap projection="geoAlbersUsa">
-        <Geographies geography={californiaCounties}>
-          {({ geographies }) =>
-            geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                geography={geo}
-                onMouseEnter={() => handleCountyHover(geo.properties.CountyName)}
-                onMouseLeave={handleCountyLeave}
-                fill="pink"
-                stroke="#FFF"
-                strokeWidth={0.5}
-              />
-            ))
-          }
-        </Geographies>
-      </ComposableMap>
-      <div>The county you are hovering is: {hoverName}</div>
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div style={{ position: "relative", width: "50%", height: "50%" }}>
+        <ComposableMap
+          projection="geoAlbersUsa"
+          projectionConfig={{ center: [0, 40], scale: 2300 }}
+          width={500}
+          height={500}
+          style={{ display: "block" }}
+        >
+          <Geographies
+            geography={californiaCounties}
+            style={{ transform: "translate(600px, 10px)" }}
+          >
+            {({ geographies }) =>
+              geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  onMouseEnter={() => handleCountyHover(geo.properties.CountyName)}
+                  onMouseLeave={handleCountyLeave}
+                  onMouseDown={() => handleMouseDown(geo.properties.CountyName)}
+                  fill={isHovered(geo.properties.CountyName)}
+                  stroke="#FFF"
+                  strokeWidth={0.5}
+                />
+              ))
+            }
+          </Geographies>
+        </ComposableMap>
+      </div>
+      <div style={{ marginLeft: "20px" }}>
+        The county you are hovering is: {hoverName} <br></br>
+        You just clicked: {clickedName}
+      </div>
     </div>
   );
 };
@@ -53,10 +72,9 @@ const CaliforniaMap = () => {
 
 function test1() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <p>
-            <img src={map} style={ImageStyle} alt="map of cali"/>
             Flynn testing here
           </p>
           
