251f8dd8ea334b2996368d82f77855e62e02d204
FlynnMager <flynnmager@gmail.com>
2023-03-08 18:12:02 -0800

    Rent info now displays on click
    changed csv column names to start with non numeric characters
    added new package, papaparse
    rent info now displays to users on click

--- housing/src/Flynn.js
+++ housing/src/Flynn.js
@@ -1,24 +1,50 @@
 import React from 'react';
 import './App.css';
-
+import Papa from 'papaparse';
 import {
   ComposableMap,
   Geographies,
   Geography,
 } from 'react-simple-maps';
 import californiaCounties from './California_County_Boundaries.geojson';
-
+import data from './counties - Sheet1.csv';
 
 
 const CaliforniaMap = () => {
+  const [parsedData, setData] = React.useState([]);
+  React.useEffect(() => {
+    Papa.parse(data, {
+      download: true,
+      header: true,
+      complete: function(results) {
+        setData(results.data);
+      }
+    });
+  }, []);
   const [hoverName, setTooltipContent] = React.useState('No county hovered');
   const [clickedName, setClickedName] = React.useState('No county clicked yet');
+  const [zero, setZero]= React.useState('');
+  const [one, setOne]= React.useState('');
+  const [two, setTwo]= React.useState('');
+  const [three, setThree]= React.useState('');
+  const [four, SetFour]= React.useState('');
+  const [avg, setAvg]= React.useState('');
+  const [divStyle, setDivStyle]= React.useState('hidden');
   const handleCountyHover = (CountyName) => {
     console.log(CountyName);
     setTooltipContent(CountyName);
   };
   const handleMouseDown = (CountyName) => {
     //call function to display the county rent info
+    const rentPrice = parsedData.find(county => county.County === (CountyName+" County"));
+    setZero(rentPrice.ZEROBR);
+    setOne(rentPrice.ONEBR);
+    setTwo(rentPrice.TWOBR);
+    setThree(rentPrice.THREEBR);
+    SetFour(rentPrice.FOURBR);
+    setAvg(rentPrice.AVG);
+    setDivStyle('visible');
+    console.log(divStyle);
     setClickedName(CountyName);
   };
   const handleCountyLeave = () => {
@@ -64,7 +90,18 @@ const CaliforniaMap = () => {
       <div style={{ marginLeft: "20px" }}>
         The county you are hovering is: {hoverName} <br></br>
         You just clicked: {clickedName}
+        <br></br>
+        <div style={{visibility: divStyle}}>
+        The rent data for {clickedName} County is as follows: <br></br>
+        Average Rent Cost : ${avg} USD <br></br>
+        Zero Bedrooms: ${zero} USD <br></br>
+        One Bedroom: ${one} USD <br></br>
+        Two Bedrooms: ${two} USD <br></br>
+        Three Bedrooms: ${three} USD <br></br>
+        Four Bedrooms: ${four} USD <br></br>
+        </div>
       </div>
+      
     </div>
   );
 };
