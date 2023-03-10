070b911ff2b1396efd25f80ff1572a97eb21280f
Chloe DePaul <cdepaul12@ucla.edu>
2023-03-09 21:32:19 -0800

    Basic signup page outline/some login adjustments

--- housing/src/App.js
+++ housing/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
 import { LogIn } from "./LogIn";
+import { SignUp } from "./SignUp";
 
 function App() {
+  const [currForm, setCurrForm] = useState('login');
+
+  const switchForm = (formName) => {
+    setCurrForm(formName);
+  }
+
   return (
     <div className="App">
-        <LogIn />
+      {
+        currForm === "login" ? <LogIn onFormSwitch={switchForm} /> : <SignUp onFormSwitch={switchForm} />
+      }
+        
     </div>
   );
 }
--- housing/src/SignUp.jsx
+++ housing/src/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 
-export const LogIn = (props) => {
+export const SignUp = (props) => {
     const [email, setEmail] = useState('');
     const [pswd, setPswd] = useState('');
+    const [name, setName] = useState('');
 
     const submitLogin = (i) => {
         i.preventDefault();
@@ -12,13 +13,16 @@ export const LogIn = (props) => {
     return (
         <div>
         <form onSubmit={submitLogin}>
+            <label>Full Name</label>
+            <input value={name} onChange={(i) => setName(i.target.value)} name="name" id="name" placeholder="i.e. John Smith" />
             <label for="email">Email </label>
             <input value={email} onChange={(i) => setEmail(i.target.value)} type="email" placeholder="i.e. youremail@gmail.com" id="email" name="email"/>
             <label for="password">Password </label>
             <input value={pswd} onChange={(i) => setPswd(i.target.value)} type="password" placeholder="********" id="password" name="password"/>
             <button type="submit"> Log In</button>
         </form>
-        <button onClick={() => props.onFormSwitch('signup')}>Don't have an account? Sign up here</button>
+        <button onClick={() => props.onFormSwitch('login')}>Already have an account? Sign in here</button>
         </div>
+
     )
 }
