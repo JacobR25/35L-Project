ec810e855b30d2cdf6e3cdcd748f2fdf54ae8ef5
Chloe DePaul <cdepaul12@ucla.edu>
2023-03-11 11:48:09 -0800

    must have valid email/password

--- housing/src/LogIn.jsx
+++ housing/src/LogIn.jsx
@@ -3,22 +3,49 @@ import React, { useState } from "react";
 export const LogIn = (props) => {
     const [email, setEmail] = useState('');
     const [pswd, setPswd] = useState('');
+    const [validPswd, setValidP] = useState(false);
+    const [validEmail, setValidE] = useState(false);
+
+    const emailCheck = (email) => {
+        // chceck if the email is already in our data base
+        setValidE(validEmail => validEmail = true);
+    }
+
+    const pswdCheck = (pswd) => {
+        setValidP(validPswd => validPswd = true);
+        emailCheck(email);
+        if(validEmail){
+        // check if the pswd matched the email
+        } else {
+            setValidP(validPswd => validPswd = false);
+        }
+    }
 
     const submitLogin = (i) => {
         i.preventDefault();
-        console.log(email);
+        pswdCheck(pswd);
+        if(validPswd){
+            if(email && pswd) {
+                localStorage.setItem("emailID", email);
+                localStorage.setItem("pswdID", pswd);
+                alert("Welcome Back to House Helpers!");
+            }
+        }
     }
 
     return (
-        <div className="formatting-container">
-        <form className="login-format" onSubmit={submitLogin}>
-            <label for="email">Email </label>
-            <input value={email} onChange={(i) => setEmail(i.target.value)} type="email" placeholder="i.e. youremail@gmail.com" id="email" name="email"/>
-            <label for="password">Password </label>
-            <input value={pswd} onChange={(i) => setPswd(i.target.value)} type="password" placeholder="********" id="password" name="password"/>
-            <button type="submit"> Log In</button>
-        </form>
-        <button className="link-button" onClick={() => props.onFormSwitch('signup')}>Don't have an account? Sign up here</button>
+        <div className="App">
+            <div className="formatting-container">
+                <h2 className="subTitle">Welcome Back!</h2>
+            <form className="login-format" onSubmit={submitLogin}>
+                <label for="email">Email </label>
+                <input value={email} onChange={(i) => setEmail(i.target.value)} type="email" placeholder="i.e. youremail@gmail.com" id="email" name="email"/>
+                <label for="password">Password </label>
+                <input value={pswd} onChange={(i) => setPswd(i.target.value)} type="password" placeholder="********" id="password" name="password"/>
+                <button type="submit"> Log In</button>
+            </form>
+            <button className="link-button" onClick={() => props.onFormSwitch('signup')}>Don't have an account? Sign up here</button>
+            </div>
         </div>
     )
 }
--- housing/src/SignUp.jsx
+++ housing/src/SignUp.jsx
@@ -4,13 +4,19 @@ export const SignUp = (props) => {
     const [email, setEmail] = useState('');
     const [pswd, setPswd] = useState('');
     const [name, setName] = useState('');
-    const [validPswd, setValid] = useState(false);
+    const [validPswd, setValidP] = useState(false);
+    const [validEmail, setValidE] = useState(false);
+
+    const emailCheck = (email) => {
+        // make sure that the email is not already associated with an account
+        setValidE(validEmail => validEmail = true);
+    }
 
     const pswdCheck = (pswd) => {
-        setValid(validPswd => validPswd = true);
+        setValidP(validPswd => validPswd = true);
         if(pswd.length < 8){
             alert("Password must contain at least 8 characters");
-            setValid(validPswd => validPswd = false);
+            setValidP(validPswd => validPswd = false);
         } else {
             var LLCount = 0;
             var ULCount = 0;
@@ -29,24 +35,25 @@ export const SignUp = (props) => {
             }
             if(otherCount === 0){
                 alert("Password must contain a special character");
-                setValid(validPswd => validPswd = false);
+                setValidP(validPswd => validPswd = false);
             } else if(numCount === 0){
                 alert("Password must contain a number");
-                setValid(validPswd => validPswd = false);
+                setValidP(validPswd => validPswd = false);
             } else if(ULCount === 0){
                 alert("Password must contain an uppercase letter");
-                setValid(validPswd => validPswd = false);
+                setValidP(validPswd => validPswd = false);
             } else if(LLCount === 0){
                 alert("Password must contain a lowercase letter");
-                setValid(validPswd => validPswd = false);
+                setValidP(validPswd => validPswd = false);
             }
         }
     }
 
     const submitLogin = (i) => {
         i.preventDefault();
+        emailCheck(email);
         pswdCheck(pswd);
-        if(validPswd){
+        if(validEmail && validPswd){
             if(name && email && pswd) {
                 localStorage.setItem("nameID", name);
                 localStorage.setItem("emailID", email);
@@ -57,18 +64,20 @@ export const SignUp = (props) => {
     }
 
     return (
-        <div className="formatting-container">
-        <form className="signup-format" onSubmit={submitLogin}>
-            <label>Full Name</label>
-            <input value={name} onChange={(i) => setName(i.target.value)} name="name" id="name" placeholder="i.e. John Smith" />
-            <label for="email">Email </label>
-            <input value={email} onChange={(i) => setEmail(i.target.value)} type="email" placeholder="i.e. youremail@gmail.com" id="email" name="email"/>
-            <label for="password">Password </label>
-            <input value={pswd} onChange={(i) => setPswd(i.target.value)} type="password" placeholder="********" id="password" name="password"/>
-            <button type="submit"> Log In</button>
-        </form>
-        <button className="link-button" onClick={() => props.onFormSwitch('login')}>Already have an account? Sign in here</button>
+        <div className="App">
+            <div className="formatting-container">
+                <h2 className="subTitle">Let's Get Started</h2>
+            <form className="signup-format" onSubmit={submitLogin}>
+                <label>Full Name</label>
+                <input value={name} onChange={(i) => setName(i.target.value)} name="name" id="name" placeholder="i.e. John Smith" />
+                <label for="email">Email </label>
+                <input value={email} onChange={(i) => setEmail(i.target.value)} type="email" placeholder="i.e. youremail@gmail.com" id="email" name="email"/>
+                <label for="password">Password </label>
+                <input value={pswd} onChange={(i) => setPswd(i.target.value)} type="password" placeholder="********" id="password" name="password"/>
+                <button type="submit"> Log In</button>
+            </form>
+            <button className="link-button" onClick={() => props.onFormSwitch('login')}>Already have an account? Sign in here</button>
+            </div>
         </div>
-
     )
 }
